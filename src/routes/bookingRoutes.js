ba50bfbb440d4323abd1c9c5f1cfa1058762c45e
meetsaurabhh <161729218+meetsaurabhh@users.noreply.github.com>
2026-04-02 22:21:18 +0530

    Delete src directory

--- src/routes/bookingRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getMyBookings, createBooking } = require("../controllers/bookingController");
-const { auth } = require("../middlewares/authMiddleware");
-
-router.get("/me", auth, getMyBookings);
-router.post("/", auth, createBooking);
-
-module.exports = router;
